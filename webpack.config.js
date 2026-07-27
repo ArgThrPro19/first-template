c6efa0a160b8741efa6d7fd0c4f8b90d83b6724d
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-07-27 18:05:40 -0400

    Added config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+import devConfig from "./webpack.dev.js";
+import prodConfig from "./webpack.prod.js";
+
+export default (env, argv) => {
+    if (argv.mode === "production") {
+        return prodConfig;
+    }
+
+    return devConfig;
+};
