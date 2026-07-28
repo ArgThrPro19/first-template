83105dd9a428fac5f191a4788b7605b2db3b5b47
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-07-28 19:33:39 -0400

    Add globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 // eslint.config.js
 import { defineConfig, includeIgnoreFile } from "eslint/config";
+import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-
+import globals from "globals";
 const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 export default defineConfig([
   includeIgnoreFile(gitignorePath, { gitignoreResolution: true }),
   js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser, 
+        ...globals.node, 
+      },
+    },
+  },
 ]);
